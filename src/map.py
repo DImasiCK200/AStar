5c42321a9a863936038ab4ebe121a4d434e0655b
ShpkUshnk <zgfzgfzgaf@gmail.com>
2026-03-19 21:19:19 +0700

    initial commit

---
+++ src/map.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Map:
+  def __init__(self, start, end, block):
+    self.map = []
+    self.start = start
+    self.end = end
+    
---
+++ src/view.py
@@ -0,0 +1,6 @@
+class View:
+  def __init__(self, map):
+    self.map = map
+
+  def showMap():
+    print()
